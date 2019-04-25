73b6e17f4739b6811dc4909c20f9526e571f67ee
Morten D. Hansen <morten@visia.dk>
2019-04-25 14:43:43 +0200

    Initial commit

---
+++ src/GenerateDocblock.php
@@ -0,0 +1,175 @@
+<?php
+
+use Barryvdh\Reflection\DocBlock;
+use Barryvdh\Reflection\DocBlock\Context;
+use Barryvdh\Reflection\DocBlock\Tag;
+use Barryvdh\Reflection\DocBlock\Serializer as DocBlockSerializer;
+
+class genModelsDocBlock
+{
+    private $properties;
+    private $nullableColumns;
+    private $reflection;
+    private $model;
+
+    public function __construct($model)
+    {
+        $this->model = $model;
+    }
+
+    public function createDocs()
+    {
+        $this->reflection = new \ReflectionClass($this->model);
+        $this->getPropertiesFromTable($this->model);
+
+        return $this->createPhpDocs($this->model);
+
+    }
+
+    /**
+     * Load the properties from the database table.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     */
+    protected function getPropertiesFromTable($model)
+    {
+        $table = $model->getConnection()->getTablePrefix() . $model->getTable();
+        $schema = $model->getConnection()->getDoctrineSchemaManager($table);
+        $databasePlatform = $schema->getDatabasePlatform();
+        $databasePlatform->registerDoctrineTypeMapping('enum', 'string');
+
+        $database = null;
+        if (strpos($table, '.')) {
+            list($database, $table) = explode('.', $table);
+        }
+        $columns = $schema->listTableColumns($table, $database);
+        if ($columns) {
+            foreach ($columns as $column) {
+                $name = $column->getName();
+                if (in_array($name, $model->getDates())) {
+                    $type = '\Illuminate\Support\Carbon';
+                } else {
+                    $type = $column->getType()->getName();
+                    switch ($type) {
+                        case 'string':
+                        case 'text':
+                        case 'date':
+                        case 'time':
+                        case 'guid':
+                        case 'datetimetz':
+                        case 'datetime':
+                            $type = 'string';
+                            break;
+                        case 'integer':
+                        case 'bigint':
+                        case 'smallint':
+                            $type = 'integer';
+                            break;
+                        case 'boolean':
+                            switch ('mysql') {
+                                case 'sqlite':
+                                    $type = 'integer';
+                                    break;
+                                case 'mysql':
+                                default:
+                                    $type = 'boolean';
+                                    break;
+                            }
+                            break;
+                        case 'decimal':
+                        case 'float':
+                            $type = 'float';
+                            break;
+                        default:
+                            $type = 'mixed';
+                            break;
+                    }
+                }
+                $comment = $column->getComment();
+                if (!$column->getNotnull()) {
+                    $this->nullableColumns[$name] = true;
+                }
+                $this->setProperty($name, $type, true, true, $comment, !$column->getNotnull());
+
+            }
+        }
+    }
+
+    /**
+     * @param string $name
+     * @param string|null $type
+     * @param bool|null $read
+     * @param bool|null $write
+     * @param string|null $comment
+     * @param bool $nullable
+     */
+    protected function setProperty($name, $type = null, $read = null, $write = null, $comment = '', $nullable = false)
+    {
+        if (!isset($this->properties[$name])) {
+            $this->properties[$name] = array();
+            $this->properties[$name]['type'] = $type;
+            $this->properties[$name]['read'] = false;
+            $this->properties[$name]['write'] = false;
+            $this->properties[$name]['comment'] = (string)$comment;
+        }
+
+        if ($read !== null) {
+            $this->properties[$name]['read'] = $read;
+        }
+        if ($write !== null) {
+            $this->properties[$name]['write'] = $write;
+        }
+    }
+
+    /**
+     * @param string $class
+     * @return string
+     */
+    protected function createPhpDocs($class)
+    {
+
+        /** @var \ReflectionClass $reflection */
+        $reflection = $this->reflection;
+        $namespace = $reflection->getNamespaceName();
+
+        $phpdoc = new DocBlock($reflection, new Context($namespace));
+        if (!$phpdoc->getText()) {
+            $phpdoc->setText($class);
+        }
+        $properties = array();
+        $methods = array();
+        foreach ($phpdoc->getTags() as $tag) {
+            $name = $tag->getName();
+            if ($name == "property" || $name == "property-read" || $name == "property-write") {
+                $properties[] = $tag->getVariableName();
+            } elseif ($name == "method") {
+                $methods[] = $tag->getMethodName();
+            }
+        }
+        foreach ($this->properties as $name => $property) {
+            $name = "\$$name";
+            if (in_array($name, $properties)) {
+                continue;
+            }
+            if ($property['read'] && $property['write']) {
+                $attr = 'property';
+            } elseif ($property['write']) {
+                $attr = 'property-write';
+            } else {
+                $attr = 'property-read';
+            }
+
+            $tagLine = trim("@{$attr} {$property['type']} {$name} {$property['comment']}");
+            $tag = Tag::createInstance($tagLine, $phpdoc);
+            $phpdoc->appendTag($tag);
+        }
+
+        $serializer = new DocBlockSerializer();
+        $serializer->getDocComment($phpdoc);
+        $docComment = $serializer->getDocComment($phpdoc);
+
+        $output = "\n{$docComment}\n\n";
+        return $output;
+    }
+}
+
